0eff3707d9dbe7ff0bb9505beb7d78ee6ab551ab
najem2002573 <najemzoabie40@gmail.com>
2025-03-26 19:53:14 +0200

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
-/*import 'package:app/intro%20pages/activities.dart';
-import 'package:app/intro%20pages/agepage.dart';
-import 'package:app/intro%20pages/colestrol.dart';
-import 'package:app/intro%20pages/currentlevel.dart';
-import 'package:app/intro%20pages/heightpage.dart';
-import 'package:app/intro%20pages/weightpage.dart';
-import 'package:app/pages/calories.dart';
-*/
-import 'package:app/intro%20pages/weightpage.dart';
-import 'package:app/pages/workout.dart';
 
-import 'package:app/intro%20pages/genderpage.dart';
-import 'package:app/pages/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-//import 'package:app/pages/login.dart';
-//import 'package:app/pages/register.dart';
 import 'package:app/pages/home.dart';
-//import 'package:app/intro%20pages/goalscreen.dart';
+
 //the app <??<>??>
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensures Firebase initializes properly
